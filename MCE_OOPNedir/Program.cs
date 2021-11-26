77853813774347114f11600966ae6994cc99aa2e
jackup <yk.58@hotmail.com>
2021-11-26 17:02:41 +0300

    Add project files.

---
+++ MCE_OOPNedir/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCE_OOPNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
